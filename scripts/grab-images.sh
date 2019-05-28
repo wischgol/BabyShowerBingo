58dc68d38617ae30d076637cdbbf93946410e71b
Thomas Wischgoll <thomas@himalia.wischgoll.tk>
2019-05-27 22:19:43 -0400

    scaling and centering of images now is done with OpenCV directly

--- scripts/grab-images.sh
+++ scripts/grab-images.sh
@@ -3,7 +3,7 @@
 let count=0
 
 for f in *_files/*.jpg; do
-    #cp "$f" image-$count.jpg
-    jpegtopnm "$f" | pamscale -width=220 -height=220 | pnmtojpeg > image-$count.jpg
+    cp "$f" images/image-$count.jpg
+    #jpegtopnm "$f" | pamscale -width 220 -height 220 | pnmtojpeg > images/image-$count.jpg
     let count=count+1
 done
